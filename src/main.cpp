ae67cc668920e465ea164cb80a74e2d173eb1e66
weerachai <from_weerachai@hotmail.com>
2023-07-30 23:24:43 +0700

    update code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <Arduino.h>
-// #include <TFT_eSPI.h>
-// #include <SPI.h>
 #include <SoftwareSerial.h>
 
 // DI
 int io1 = 13; // D13
-int io2 = 12; // D12
-int io3 = 14; // D14
-int io4 = 27; // D27
-int io5 = 26; // D26
+// int io2 = 12; // D12 do not use
+int io2 = 14; // D14
+int io3 = 27; // D27
+int io4 = 26; // D26
+int io5 = 33; // D33
 int io6 = 25; // D25
 
 // DQ
@@ -24,99 +23,59 @@ int rx_1 = 4;
 int tx_2 = 18;
 int rx_2 = 19;
 
-String readString = "";
-// String input = "504-A"; 
-String read_card; 
-
-const int BUFFER_SIZE = 50;
-char buf[BUFFER_SIZE];
+String read_card = "";
+String read_barcode = "";
 
 bool off = 1;
 bool on = 0;
 
 SoftwareSerial gm65(rx_1, tx_1); // RX, TX
-// TFT_eSPI tft = TFT_eSPI();
-
-/*
-Program
-1.000
-2.100
-3.110
-4.111
-5.001
-6.011
-7.010
-8.101
-*/
 
 String read_kanban(){
   String input; 
-  if (digitalRead(io1) && digitalRead(io2) && digitalRead(io3)) // 000
+  // while (!digitalRead(io1) && !digitalRead(io2) && !digitalRead(io3) or digitalRead(io1) && digitalRead(io2) && digitalRead(io3))
+  // {
+  //   digitalWrite(cutoff_sewing, off); // barcode off 
+  //   digitalWrite(alarm_on, off); // alarm off 
+  // }
+  
+  if (!digitalRead(io1) && digitalRead(io2) && digitalRead(io3)) // 100
   {
-    if (!digitalRead(io1) && !digitalRead(io2) && !digitalRead(io3)) // 111
-    {
-      if (digitalRead(io1) && !digitalRead(io2) && !digitalRead(io3)) // 100
-      {
-        input = "100";
-      }
-      else if (digitalRead(io1) && digitalRead(io2) && !digitalRead(io3)) // 110
-      {
-        input = "110";
-      }
-      else if (!digitalRead(io1) && !digitalRead(io2) && digitalRead(io3)) // 001
-      {
-        input = "001";
-      }
-      else if (!digitalRead(io1) && digitalRead(io2) && digitalRead(io3)) // 011
-      {
-        input = "011";
-      }
-      else if (!digitalRead(io1) && digitalRead(io2) && !digitalRead(io3)) // 010
-      {
-        input = "010";
-      }
-      else if (digitalRead(io1) && !digitalRead(io2) && digitalRead(io3)) // 101
-      {
-        input = "101";
-      }
-    }    
-  } 
+    input = "504-A\r";
+  }
+  else if (!digitalRead(io1) && !digitalRead(io2) && digitalRead(io3)) // 110
+  {
+    input = "4519001660732\r";
+  }
+  else if (digitalRead(io1) && digitalRead(io2) && !digitalRead(io3)) // 001
+  {
+    input = "4519001660735\r";
+  }
+  else if (digitalRead(io1) && !digitalRead(io2) && !digitalRead(io3)) // 011
+  {
+    input = "011";
+  }
+  else if (digitalRead(io1) && !digitalRead(io2) && digitalRead(io3)) // 010
+  {
+    input = "010";
+  }
+  else if (!digitalRead(io1) && digitalRead(io2) && !digitalRead(io3)) // 101
+  {
+    input = "bypass";
+  }
   return input; 
 }
-// String inputString = "";      // ตัวแปรสำหรับเก็บค่าจากตัวแสกนเนอร์
-// String DataScanner = "";      //ตัวแปรสำหรับเก็บค่าในการประมวลผล
-// bool stringComplete = false;  // ตัวแปรเช็คว่ารับข้อมูลครบแล้วหรือยัง
 
-// void serialEvent() {
-//   while (Serial.available()) {
-//     // get the new byte:
-//     char inChar = (char)Serial.read();
-//     // add it to the inputString:
-//     inputString += inChar;
-//     // if the incoming character is a newline, set a flag so the main loop can
-//     // do something about it:
-//     if (inChar == '\n') {
-//       stringComplete = true;
-//       DataScanner = String(inputString);
-//     }
-//   }
-// }
-void tring (int x){
-  digitalWrite(x, 0);
-  delay(5);
-  digitalWrite(x, 1);
-  delay(5);
-  Serial.println("tring on");
-}
+
 void setup(void) {
   Serial.begin(115200);
   while (!Serial) ;
   gm65.begin(9600);
-  // pinMode(io1, INPUT);
+  pinMode(io1, INPUT);
   pinMode(io2, INPUT);
   pinMode(io3, INPUT);
   pinMode(io4, INPUT);
-  pinMode(io5, INPUT);
+  // pinMode(io5, INPUT);
   pinMode(io6, INPUT);
 
   pinMode(trig1, OUTPUT);
@@ -125,56 +84,96 @@ void setup(void) {
   pinMode(cutoff_sewing, OUTPUT);
   pinMode(DO_SPARE, OUTPUT);
   
-  digitalWrite(cutoff_sewing, off);
-  digitalWrite(alarm_on, off);
-  digitalWrite(trig1, 1);
-  // delay(5);
+  // digitalWrite(cutoff_sewing, on);
+  // digitalWrite(alarm_on, off);
+  // digitalWrite(trig1, 1);
+  // delay(1000);
 
 }
 void loop() {
-  read_card = read_kanban();
-  // digitalWrite(trig1, 1); // barcode off
+  digitalWrite(trig1, 1); // barcode off
   digitalWrite(cutoff_sewing, off); // barcode off 
   digitalWrite(alarm_on, off); // alarm off 
-
-  while (!digitalRead(io6)) // check thread on
-  {
-    digitalWrite(trig1, 1); // barcode off
-  }  
-
+  
   while (digitalRead(io6)) // check thread on
   {
+    // if no card
+    while (!digitalRead(io1) && !digitalRead(io2) && !digitalRead(io3) || digitalRead(io1) && digitalRead(io2) && digitalRead(io3)) // no card
+    {
+      digitalWrite(trig1, 1); // barcode off
+      digitalWrite(cutoff_sewing, off); // barcode off 
+      digitalWrite(alarm_on, off); // alarm off 
+    }
+    // Mode by pass
+    while (read_kanban() == "bypass")
+    {
+      digitalWrite(trig1, 1); // barcode off
+      digitalWrite(cutoff_sewing, on); // barcode on
+      digitalWrite(alarm_on, off); // alarm off 
+      if (!digitalRead(io1) && !digitalRead(io2) && !digitalRead(io3) || digitalRead(io1) && digitalRead(io2) && digitalRead(io3) || !digitalRead(io6)) // eject card and thread
+        {
+          read_card = "";
+          read_barcode = "";
+          digitalWrite(cutoff_sewing, off); // barcode off
+          break;
+        } 
+    }
+
+    // thread and card ready
     digitalWrite(trig1, 0); // barcode on
 
     if (gm65.available()) //Checking if any data has been received
     {
       while (gm65.available()) //Read bit by bit until UART buffer is empty
       {
-        readString = gm65.readString();
-        Serial.print(readString); //Display input variable
+        read_card = read_kanban(); // read card
+        read_barcode = gm65.readString(); // read bar code
+        
+        // int len_card = read_card.length();
+        // int len_bar = read_barcode.length();
 
-        if (readString.compareTo("504-A")> 0)
+        // if (read_barcode.compareTo(read_card) = 0)
+        
+        if (read_barcode == read_card)
         {
-          Serial.print(readString);
+          Serial.println(read_barcode);
+          Serial.println(read_card);
+          // Serial.print("barcode : ");  Serial.println(len_bar);
+          // Serial.print("card : ");  Serial.println(len_card);
+
           while (digitalRead(io6))
           {
             digitalWrite(trig1, 1); // barcode off 
             digitalWrite(alarm_on, off); // alarm off 
             digitalWrite(cutoff_sewing, on); // sewing on 
+            if (!digitalRead(io1) && !digitalRead(io2) && !digitalRead(io3) || digitalRead(io1) && digitalRead(io2) && digitalRead(io3)) // eject card
+            {
+              read_card = "";
+              read_barcode = "";
+              break;
+            }
           }          
         }else{
-          Serial.print("no : ");
-          Serial.print(readString);
+          Serial.print("no : ");  Serial.println(read_barcode);
+          Serial.print("no : ");  Serial.println(read_card);
+          // Serial.print("barcode : ");  Serial.println(len_bar);
+          // Serial.print("card : ");  Serial.println(len_card);
+
           while (digitalRead(io6))
           {
             digitalWrite(trig1, 1); // barcode off
             digitalWrite(alarm_on, on); // alarm on 
             digitalWrite(cutoff_sewing, off); // sewing off 
+            if (!digitalRead(io1) && !digitalRead(io2) && !digitalRead(io3) || digitalRead(io1) && digitalRead(io2) && digitalRead(io3)) // eject card
+            {
+              read_card = "";
+              read_barcode = "";
+              break;
+            }
           }
         }
-        delay(1);
+        delay(5);
       } // end while Read bit by bit until UART buffer is empty
-      Serial.println(); //Move to a new line
     } // end if Checking if any data has been received 
   } // end while check thread on
 } // end loop
